a45a285ddf5d06e5cb38d9f72b262c8d3824e177
lastchancebar <darinaduffykelly@gmail.com>
2022-03-13 16:10:26 +0000

    save  highscores to local storageand array htop 5 ighscores in order

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,7 +1,9 @@
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("savescoreBtn");
 const mostRecentScore = localStorage.getItem('mostRecentScore');
-const finalScore = document.getElementById('finlaScore');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const MAX_HIGH_SCORES = 5;
+const finalScore = document.getElementById('finalScore');
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
@@ -12,4 +14,16 @@ username.addEventListener("keyup", () => {
 saveHighScore = e => {
     console.log("clicked the save button!");
     e.preventDefault();
+
+    const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value
+        };
+        highScores.push(score);
+    highScores.sort( (a,b) => b.score -a.score);
+    highScores.splice(5);
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
+
+    
 };
